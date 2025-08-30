3b8d21a76dcab5c2b51f378289957d592bb61f8d
Dinuka Pramod <dinukah98@gmail.com>
2025-08-30 17:59:40 +0530

    input output lesson added

---
+++ day2/7_inout.py
@@ -0,0 +1,53 @@
+#to get an input, use 'input' keyword ✅
+#fullName = input("Enter your full name: ")
+#print("Your name is",fullName)
+
+#python automatically adds a space around the inputted value when printing ✅
+#fname = input("Enter first name: ")
+#lname = input("Enter last name: ")
+#print("Your first name is",fname,"and last name is",lname)
+
+#default type is String, but can be convereted later ✅
+#age = int(input("How old are you: "))
+#print(f'you are {age}')
+#print(type(age)) 
+ # this will give 'class str' if removed the 'int' part ✅
+#print("You will be",age+1,"years old next year")
+
+#what happens when a user entered a wrong typed input here? ❌
+
+
+#output by print
+#also discussed in day1 > 5_string_methods  ✅
+
+#placeholders with {} ❌
+a = 123
+b= "random"
+print('value of a is {} and b is {}',format(a,b))
+
+#print sevaral vars ✅
+name = "cat"
+age = 25
+print(-123.0,name,25*3,age)
+
+#change default separator ✅
+print(-123.0,name,25*3,age, sep="X")
+print(-123.0,name,25*3,age, sep="--")
+
+#end ❌
+print(-123.0,name,25*3,age, sep="--",end="$")
+
+#star patterns ❌
+
+
+
+
+
+
+
+
+
+
+
+
+#python 7_inout.py
