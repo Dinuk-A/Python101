0c25a4ae5e9612c24ee127fb91643df93ca13d50
Dinuka Pramod <dinukah98@gmail.com>
2025-09-21 14:13:22 +0530

    error handling lessons added

--- day3/2_errors.py
+++ day3/2_errors.py
@@ -1,4 +1,4 @@
-#major error types in PY
+#major error types in PY✅
 '''
 Syntax errors = mistakes in keywords/language
 Runtime errors = errors found when the script is executed
@@ -14,4 +14,44 @@ Major runtime errors
     5. Module not found errors = when try to import non existing modules/funtion
 '''
 
-#to prevent these and more, use try-execpt blocks ✅
+#to prevent these and more, use try-except blocks ✅
+try:
+    x = 100/0
+except:
+    print("deviding by 0 is not allowed")
+    
+#instead of looking for all the errors, we can catch a specific error ✅
+try:
+    y = 10/0
+except ZeroDivisionError :
+    print("deviding by 0 is not allowed")
+    
+    
+#we can handle different errors in different ways ✅
+try:
+    num = int("hello")  
+    result = 10 / num
+except ValueError:
+    print("Invalid number!")
+except ZeroDivisionError:
+    print("Division by zero!")
+    
+#if no errors occur, the code in the else block is executed ✅
+try:
+    x = 10 / 2
+except ZeroDivisionError:
+    print("Division by zero!")
+else:
+    print("Success! Result is:", x)
+    
+
+#finally block is always executed ✅
+try:
+    x = 10 / 0
+except ZeroDivisionError:
+    print("Division by zero!")
+finally:
+    print("Execution completed.")
+
+    
+# python 2_errors.py
