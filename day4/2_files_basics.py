140fdb72c40a5f9c0a237e50c2d6c2d80acffe0c
Dinuka Pramod <dinukah98@gmail.com>
2025-09-28 15:25:38 +0530

    files basics updated

--- day4/2_files_basics.py
+++ day4/2_files_basics.py
@@ -10,43 +10,51 @@ file.close()                     #always close the file after using it for any t
 
 # 4 main opening modes to handle files
 # 1. 'r' === read, the default mode (if not mentioned in the open()), returns an error if file doesnt exist
-# 2. 'w' === overwrite, create the file if desnt exist
+# 2. 'w' === overwrite, create the file if desnt exist, returns the number of characters written
 # 3. 'a' === append , add content at the end of the existing content, create file if doesnt exist
-# 4. 'x' === create, create a specific file by given name, returns an error if already exists 
+# 4. 'x' === create, create a specific file by given name, returns an error if already exists
 
 # 2 main modes which way the file should be handle
 # 1. 't' === as text, default mode
 # 2. 'b' === as binary (images)
 
 # read() for reading content ✅✅✅
+#if the given file is not found, python will automatically create a new one
+
 f = open('file_handling_exs\\sample.txt','rt')
 
 # read all content
 print('************reading all content************')
 print(f.read())
 
+#after above code, file pointer is at the end of the content
+#since we need the same file to be read in multiple examples below,we need to reset the file pointer
+f.seek(0)      
+
 # read only first set of characters of a given amount
 print('************reading first 8 characters************')
 print(f.read(8))
+f.seek(0)     
 
 #only read the first line, call the same function multiple times to read more lines
 print('************reading only line by line************')
 print(f.readline())
 print(f.readline())
+f.seek(0)     
 
 #looping through file
-print("looping through file")
+print("************')looping through file************')")
 for x in f:
     print(x)
 f.close()
-    
+
 # append ✅✅✅
 
 # the new content will be appended to the end of last line, not in a new line
 file2 = open('file_handling_exs\\myfile.txt','a')
 file2.write('now file has new content, added by python')
 
-#to add to a new line
+#to add to a new line using '\n' or '\t'
 file2.write('\nnow file has a new line, added by python')
 
 file2.close()
