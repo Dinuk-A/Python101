52fe79b633821e1a770834e59c265c83373ea609
Dinuka Pramod <dinukah98@gmail.com>
2025-11-17 19:07:07 +0530

    post with path params added

--- fastapi/lessons/l4_post_basics.py
+++ fastapi/lessons/l4_post_basics.py
@@ -56,3 +56,35 @@ def addNewPerson(person: Person):
 @router.get("/person/all")
 def getAllPeople():
     return people_db
+
+# LESSON 3 ✅
+# POST with path params
+# execution added to the lessons\l5_post_params.py
+
+user_orders = {
+    1: {"name": "Alice", "orders": []},
+    2: {"name": "Bob", "orders": []}
+}
+
+class Order(BaseModel):
+    id: int
+    item: str    
+
+@router.post("/users/{user_id}/orders")
+def add_order(user_id: int, order: Order):
+    
+    if user_id not in user_orders:
+        return {"error": "User not found"}
+
+    #only send the name (store it)
+    #user_orders[user_id]["orders"].append(order.item)
+    
+    # store full order object
+    user_orders[user_id]["orders"].append(order.model_dump())
+    
+    return {"message": "Order added", "orders": user_orders[user_id]["orders"]}
+
+# to get all new users
+@router.get("/userorders/all")
+def getAllUserOrders():
+    return user_orders
---
+++ fastapi/lessons/l5_post_params.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+BASE_URL = "http://127.0.0.1:8000"
+
+def test_path_param():
+    """POST with path parameter"""
+    print("=== POST with PATH parameter ===\n")
+    
+    order_data = {"id": 1, "item": "cat"}
+    
+    response = requests.post(
+        f"{BASE_URL}/users/1/orders",
+        json=order_data
+    )
+    print(f"Response: {json.dumps(response.json(), indent=2)}\n")
+
+# fn to get all users (GET request) 
+def get_all_users():
+    """Retrieve all users (GET request)"""
+    response = requests.get(f"{BASE_URL}/userorders/all")
+    print(f"\n📋 All users:\n{json.dumps(response.json(), indent=2)}")
+    
+if __name__ == "__main__":
+    test_path_param()
+    get_all_users()
+    
+#to run this > >> python lessons\l5_post_params.py
+    
